f0792778def1f8fddac0096d47b3bbf5f0f6b492
Tri Wahyuni <105841118423@student.unismuh.ac.id>
2026-01-13 09:07:18 +0800

    add pznpipeline

---
+++ vars/pznPipeline.groovy
@@ -0,0 +1,19 @@
+def call(Map config {
+    if(config.type = "maven") {
+        mavenPipeline()
+
+    } else {
+        pipeline {
+            agent any
+            stages {
+                stage("unsupported Type") {
+                    steps {
+                        script {
+                            echo("Unsupported Type")
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
